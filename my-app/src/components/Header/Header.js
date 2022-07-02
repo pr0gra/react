926a225f347e5adc09aa2fd676399a7ed0cc46ba
pr0gra <denisovProg@gmail.com>
2022-07-02 22:41:22 +0500

    comment

--- my-app/src/components/Header/Header.js
+++ my-app/src/components/Header/Header.js
@@ -1,26 +1,28 @@
 import styles from "./Header.module.css";
 import React from "react";
-import mainLogo from '../../assets/images/logo.svg'
-import lightIcon from '../../assets/icons/light.svg'
-import menuIcon from '../../assets/icons/menu.svg'
-
+import mainLogo from "../../assets/images/logo.svg";
+import themeIcon from "../../assets/icons/light.svg";
+import menuIcon from "../../assets/icons/menu.svg";
 
 export default function Header() {
   return (
-    <header className={styles["header"]}>
-      <div className={styles["logo"]}>
-        <img src={mainLogo} alt="" />
+    <header className={styles["mobile-header"]}>
+        <a href="/">
+          <img src={mainLogo} alt="Digiency" />
+        </a>
+
+
+      <div className={styles["theme"]}>
+        <button className={styles["header-button"]}>
+          <img src={themeIcon} alt="Chenge Theme" />
+        </button>
       </div>
 
-      <div className={styles["light"]}><img src={lightIcon} alt="" /></div>
-
-      <div className={styles["menu"]}> <img src={menuIcon} alt="" /></div>
+      <div className={styles["menu"]}>
+        <button className={styles["header-button"]}>
+          <img src={menuIcon} alt="Menu" />
+        </button>
+      </div>
     </header>
   );
 }
-
-
-
-
-
-
