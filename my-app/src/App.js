34682a30ab8f989ea00907c1ae99bca45be5840b
pr0gra <denisovProg@gmail.com>
2022-07-02 23:12:21 +0500

    comment

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,12 @@
-import Header from "./components/Header/Header";
-import "./App.css";
+import Header from './components/Header/Header'
+import "./App.css"
 
-
-function App() {
+function App() {  
   return (
     <>
       <Header />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
